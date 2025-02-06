7cc9527cc64952c2fe90bdcf000fa28c22c4d0e6
dineshganga1 <dineshgangavaram56@gmail.com>
2025-02-06 09:30:45 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages/Home';
+import MyGarden from './pages/MyGarden'; // Import the MyGarden component
+import PlantCare from './pages/PlantCare';
+import SeasonalPlants from './pages/SeasonalPlants';
+
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/home">
+        <Route path="/my-garden" component={MyGarden} />
+        <Route path="/plant-care" component={PlantCare} />
+        <Route path="/Seasonal-Planting" component={SeasonalPlants} />
+        
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
